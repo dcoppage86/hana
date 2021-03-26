4286758c1d2ee8ebbc70a27cddf5b2016bad526c
dcoppage86 <dakotakcoppage@me.com>
2021-03-26 17:10:15 -0600

    adding nested resources for tasks to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
-  resources :users
   root 'welcome#home'
   
+  resources :users do
+    resources :tasks
+  end
   resources :tasks
   resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
