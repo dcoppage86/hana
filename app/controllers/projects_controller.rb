f09728e126fef40b0c8dd2454594288002a52c15
dcoppage86 <dakotakcoppage@me.com>
2021-03-26 12:32:13 -0600

    last commmit was larger because i added a bootstrap layout to the application.html.erb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,34 @@
 class ProjectsController < ApplicationController
+    before_action :set_project
+
+    def index
+        @projects = Project.all
+    end
+
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+          redirect_to project_path(@project)
+        else
+          render 'new'
+        end
+    end
+    
+
+    private
+
+    def project_params
+        params.require(:project).permit(:name)
+    end
+
+    def set_project
+        @project = Project.find_by_id(params[:id])
+    end
+    
+    
+    
 end
