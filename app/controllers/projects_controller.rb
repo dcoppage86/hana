3c2f5d77e5b276f2e0d61d03ed46081a6bf9b7ff
dcoppage86 <dakotakcoppage@me.com>
2021-03-26 22:56:24 -0600

    refactoring create method in projects_controller to fit the nested resource

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,19 +2,24 @@ class ProjectsController < ApplicationController
     before_action :set_project, only: [:show]
 
     def index
-        @projects = Project.all
+        @projects = @user.projects
     end
 
     def new
-        @project = Project.new
+        @project = @user.projects.build
     end
 
     def create
-        @project = Project.new(project_params)
-        if @project.save
-          redirect_to project_path(@project)
-        else
-          render 'new'
+        @project = @user.projects.build(project_params)
+
+        respond_to do |format|
+            if @project.save
+                format.html { redirect_to user_projects_path, notice: 'Project was successfully created.'}
+                format.json { render :show, status: created, location: @project}
+            else
+                format.html { render :new }
+                format.json { render json: @project.errors, status: :unprocessable_entity}
+            end
         end
     end
 
