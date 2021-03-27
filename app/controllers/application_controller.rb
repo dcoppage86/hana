cc086ba1e538f0b25dbd581e0254b4b03ad23570
dcoppage86 <dakotakcoppage@me.com>
2021-03-26 22:08:00 -0600

    creating current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    
+    def current_user
+        if session[:user_id]
+            @current_user = User.find(session[:user_id])
+            @current_user
+        else
+            false
+        end
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    helper_method :current_user
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,11 @@ class UsersController < ApplicationController
     end
     
     def create
-        @user = User.new(user_params)
-        if @user.save
-          session[:user_id] = @user.id
-          redirect_to users_path(@user)
-        else
-          render 'new'
-        end
+        @user = User.create(user_params)
+        
+        session[:user_id] = @user.id
+        
+        redirect_to users_path(@user)
     end
 
     def show
