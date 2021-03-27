4fc20a65f2bb7c6b6d5a41d3cfdd8ae1a8a81fe9
dcoppage86 <dakotakcoppage@me.com>
2021-03-27 17:03:48 -0600

    nested resource for user projects working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+    before_action :get_user
     before_action :set_project, only: [:show, :edit, :update, :destroy]
 
     def index
@@ -16,9 +17,10 @@ class ProjectsController < ApplicationController
 
     def create
         
-        @project = Project.new(params[:name])
+        
+        @project = Project.new(project_params)
         if @project.save 
-            redirect_to project_path(@project)
+            redirect_to user_project_path(current_user, @project)
         else 
             render :new
         end
@@ -33,12 +35,18 @@ class ProjectsController < ApplicationController
 
     private
 
+    def project_params
+        params.require(:project).permit(:name, :content)
+    end
+
     def set_project
         @project = Project.find_by_id(params[:id])
     end
 
     
-    
+    def get_user
+        @user = User.find_by_id(params[:user_id])
+    end
     
     
 end
