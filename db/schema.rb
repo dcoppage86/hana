ec297984bf7cd88cae646ff0cfc1ba454b9c54c9
dcoppage86 <dakotakcoppage@me.com>
2021-03-26 22:49:53 -0600

    creating new projects form

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_191636) do
+ActiveRecord::Schema.define(version: 2021_03_27_044602) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "content"
   end
 
   create_table "tasks", force: :cascade do |t|
