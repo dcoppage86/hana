7eb46d5730a8788f5431405a304d5ac86aa0b972
dcoppage86 <dakotakcoppage@me.com>
2021-03-26 21:29:45 -0600

    nesting tasks resources under projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  # landing page
   root 'sessions#home'
+
+  # login
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
   
-  resources :users do
+  resources :users, only: [:new, :create]
+  
+  resources :projects do
     resources :tasks
   end
-  resources :tasks
-  resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
