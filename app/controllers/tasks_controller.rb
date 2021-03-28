d19864a3c7d40d4f49af0116c8db571084f2c9a2
dcoppage86 <dakotakcoppage@me.com>
2021-03-28 16:09:54 -0600

    adding index controller action to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+    before_action :login_required
+
+    def index
+        if params[:user_id]
+            if User.find(params[:user_id]) == current_user
+                @user = User.find(params[:user_id])
+                @tasks = @user.tasks 
+            else 
+                redirect_to projects_path
+                flash[:notice] = "You do not have access to this."
+            end
+        else
+            @tasks = Task.all
+        end
+    end
+    
 end
