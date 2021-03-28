f094f22e586be62c204bec4feef87f8aec4e0938
dcoppage86 <dakotakcoppage@me.com>
2021-03-28 17:35:55 -0600

    finishing tasks table in index.html.erb

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :project
   belongs_to :user
 
-  validates :name, presence :true
+  validates :name, presence: :true
 
   scope :completed_tasks, -> {where(completed: true)}
 end
