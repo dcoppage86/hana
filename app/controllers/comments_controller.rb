45f9b288cc00ed255947a8c9235a1c5c8b1c9bd3
dcoppage86 <dakotakcoppage@me.com>
2021-03-28 00:25:04 -0600

    adding comment new and create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,25 @@ class CommentsController < ApplicationController
     def index
         @comments = Comment.all
     end
+
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        if @comment.save
+          redirect_to project_comment_path(current_user, @comment)
+        else
+          render 'new'
+        end
+    end
+    
+    private
+
+    def comment_params
+        params.require(:comment).require(:subject, :content)
+    end
+    
     
 end
