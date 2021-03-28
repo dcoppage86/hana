508079eef16fc0e228ad423b45f32dd304f720c5
dcoppage86 <dakotakcoppage@me.com>
2021-03-27 22:42:42 -0600

    adding controller actions for user login:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,33 @@
 class SessionsController < ApplicationController
     def home
+
     end
 
     def new
-        
+        @user = User.new
     end
+
+    def create
+        user = User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect_to root_path => "Welcome, #{user.first_name}"
+        else
+            flash.now.alert = "Invalid email or password"
+            render "new"
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil 
+        redirect_to root_path
+    end
+
+    private
+    
+    def login(user)
+        session[:user_id] = nil
+    end
+    
     
 end
