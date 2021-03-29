b32f31e8d3f87437e2701a0149b2cc857a596ef1
dcoppage86 <dakotakcoppage@me.com>
2021-03-28 21:18:48 -0600

    adding validate presence for name and content in project:

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,7 @@ class Project < ApplicationRecord
     has_many :tasks
     has_many :comments
     has_many :users, through: :tasks
+
+    validates :name, presence: :true
+    validates :content, presence: :true
 end
