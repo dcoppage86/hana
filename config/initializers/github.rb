d2ae5e4805ff3d8f861a1490da7cb35b492628a5
dcoppage86 <dakotakcoppage@me.com>
2021-03-30 00:20:43 -0600

    adding controller action and model method for omniauth

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, ENV['GITHUB_KEY'], ENV['9d31f50b74ad213d074bffb4c37561b4ad4d3fa3']
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   end
